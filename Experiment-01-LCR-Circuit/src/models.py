a1304b964d10ce5113e57eef0e21ce7f66303a17
Aryan Bandyopadhyay <aryan.bandyopadhyay@niser.ac.in>
2025-12-15 20:50:15 +0530

    Update models.py

--- Experiment-01-LCR-Circuit/src/models.py
+++ Experiment-01-LCR-Circuit/src/models.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-def transfer_ratio(f, R, L, C):
+def transfer_ratio(frequency, R, L, C):
     """
     Voltage ratio V_R / V_i for a series RLC circuit.
     """
-    omega = 2 * np.pi * f
-    Z = np.sqrt(R**2 + (omega*L - 1/(omega*C))**2)
-    return R / Z
+    omega = 2 * np.pi * frequency
+    impedance = np.sqrt(R**2 + (omega * L - 1 / (omega * C))**2)
+    return R / impedance
 
-def phase_angle(f, R, L, C):
+
+def phase_angle(frequency, R, L, C):
     """
-    Phase angle across resistor (degrees).
+    Phase angle across the resistor (degrees).
     """
-    omega = 2 * np.pi * f
-    phi = np.arctan(R / (omega*L - 1/(omega*C)))
+    omega = 2 * np.pi * frequency
+    phi = np.arctan(R / (omega * L - 1 / (omega * C)))
     return np.degrees(phi)
